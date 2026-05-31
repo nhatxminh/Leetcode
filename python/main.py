da75611801b75ab17b30df3ca185fff6008fdcee
nhatxminh <nhatxminh@gmail.com>
2026-05-31 22:20:53 +0700

    2126

---
+++ python/main.py
@@ -0,0 +1,8 @@
+from problem_2126 import Solution as Solution2126
+
+class Main:
+    # Problem 2126
+    mass = 84
+    asteroids = [156,197,192,14,97,160,14,5]
+    solution = Solution2126()
+    print(solution.asteroidsDestroyed(mass=mass, asteroids=asteroids))
