c0c179cb001aa4b74f5b755c4fc9763252a110a7
nhatxminh <nhatxminh@gmail.com>
2026-06-05 00:40:17 +0700

    3751

---
+++ python/3751.py
@@ -0,0 +1,28 @@
+arr = []
+peak = 0
+
+for j in range(0, 10**5 + 1, 1):
+    result = 0
+    num_str = str(j)
+    if j < 100:
+        arr.append(0)
+    else: 
+        for i in range(0, len(num_str), 1):
+            if i == 0 or i == len(num_str) - 1:
+                continue
+            if num_str[i] > num_str[i - 1] and num_str[i] > num_str[i + 1]:
+                result += 1
+            elif num_str[i] < num_str[i - 1] and num_str[i] < num_str[i + 1]:
+                result += 1
+        arr.append(result + arr[j - 1])
+
+
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        return arr[num2] - arr[num1 - 1]
+    
+
+if __name__ == "__main__":
+    # print(arr)
+    solution = Solution()
+    print(solution.totalWaviness(120, 130))
