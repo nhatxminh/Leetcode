1adc0e62da596bd72cc46df3a93b21e1362d96a6
nhatxminh <nhatxminh@gmail.com>
2026-06-13 12:25:55 +0700

    236

---
+++ python/236.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, x, left: TreeNode, right: TreeNode):
+        self.val = x
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root: return None
+        if root is q or root is p: return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right: return root
+
+        return left or right
